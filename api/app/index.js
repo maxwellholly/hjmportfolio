631a47e85be494b127a667a9eca3735e98f49833
Holly Maxwell <hollymaxwell@Hollys-MacBook-Pro.local>
2020-11-22 07:03:39 -0800

    repairing Heroku build errors

--- api/app/index.js
+++ api/app/index.js
@@ -25,11 +25,11 @@ app.use("/auth", authRouter);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "../../reactjs/build")));
+  app.use(express.static(path.join(__dirname, "../../hjmportfolio-app/build")));
 
   // Handle React routing, return all requests to React app
   app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../reactjs/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../../hjmportfolio-app/build", "index.html"));
   });
 }
 
