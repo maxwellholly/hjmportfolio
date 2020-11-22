e961bf2f6be4c8b92ee40ad6e5b263f0648e4876
Holly Maxwell <hollymaxwell@Hollys-MacBook-Pro.local>
2020-11-22 09:16:07 -0800

    clean up

--- api/app/models/users.js
+++ api/app/models/users.js
@@ -2,29 +2,22 @@ module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define(
     "Users",
       {
-          id: {
-              defaultValue: DataTypes.UUIDV4,
-              primaryKey: true,
-              type: DataTypes.UUID,
-              validate: {
-                  isUUID: {args: 4, msg: "Id not valid, please try again"},
-              },
+      id: {
+          defaultValue: DataTypes.UUIDV4,
+          primaryKey: true,
+          type: DataTypes.UUID,
+          validate: {
+              isUUID: {args: 4, msg: "Id not valid, please try again"},
           },
-          username: {
-              type: DataTypes.STRING,
-              allowNull: false,
-              validate: {
-                  len: {
-                      args: [3, 500],
-                      msg: "Ask title must be at least 3 characters in length",
-                  },
-              },
-          },
-          password: {
-              type: DataTypes.STRING,
-              unique: {args: true, msg: "Sorry, this email is already in use"},
-              allowNull: {args: false, msg: "Email is required"},
-          },
+      },
+      username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+      },
+      password: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
       },
       {}
   );
