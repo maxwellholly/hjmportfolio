0b2a7e4e12d8113c66d6b963c3360c3f95e4e7c3
Holly Maxwell <hollymaxwell@Hollys-MacBook-Pro.local>
2021-10-04 22:03:19 -0700

    Resume completed

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Experience from '../../components/experience/Experience';
 
 export default class Resume extends Component {
     constructor(props) {
@@ -7,7 +8,7 @@ export default class Resume extends Component {
 
     render() {
         return (
-            <h1>Resume</h1>
+            <Experience/>
         )
     }
 }
