44f7a7a62d1869b0f917c0a84021287e27174c55
Holly Maxwell <hollymaxwell@Hollys-MacBook-Pro.local>
2021-10-12 22:09:29 -0700

    Began building projects components

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Carousel from '../../components/carousel/Carousel';
 
 export default class Projects extends Component {
     constructor(props) {
@@ -7,7 +8,7 @@ export default class Projects extends Component {
 
     render() {
         return (
-            <h1>Projects</h1>
+            <Carousel/>
         )
     }
 }
