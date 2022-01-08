2ceb5e94009c8a15b242e29b4bf1abc42a742cb4
Holly Maxwell <hollymaxwell@Hollys-MacBook-Pro.local>
2022-01-07 19:22:28 -0800

    navbar updates

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -14,7 +14,6 @@ export default function Routes(props) {
             <Route exact path={'/resume'} component={Resume} />
             <Route exact path={'/projects'} component={Projects} />
             <Route exact path={'/contact'} component={Contact} api={props.api}/>
-            <Route exact path={'/blog'} component={Blog} />
         </Switch>
     );
 }
